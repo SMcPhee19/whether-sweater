92b50d910fc3f611ec6283d76928de902060ef29
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-06-11 14:05:16 -0600

    test: finish facade testing

---
+++ spec/facades/forecast_facade_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'forcast facade' do
+  it 'can get forecast data', :vcr do
+    location = 'Denver,CO'
+    weather = ForecastFacade.new.get_forecast(location)
+
+    expect(weather).to be_a(Forcast)
+    expect(weather.id).to eq(nil)
+    expect(weather.type).to eq('forecast')
+    expect(weather.current_weather).to be_a(Hash)
+    expect(weather.daily_weather).to be_a(Array)
+    expect(weather.hourly_weather).to be_a(Array)
+  end
+end
