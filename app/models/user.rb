f3ada0e80288c17c275dd0f9f051a3749823a3ea
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-06-12 17:55:06 -0600

    chore: create user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   validates_presence_of :password_digest
 
   has_secure_password
+
+  before_create :generate_api_key
+
+  # instance methods
+  
+  def generate_api_key
+    self.api_key = SecureRandom.hex(13)
+  end
 end
